67059921b5d14442892945c822deac3c517e7691
Saur0w <sthapliyal085@gmail.com>
2026-04-09 22:27:17 +0530

    "use Lenis in page.jsx"

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,23 @@
 
 import Lenis from "lenis";
 import { useEffect } from "react";
-import Landing from "@/components/Landing";
+import Landing from "@/components/Landing/index";
 
 export default function Home() {
+    useEffect(() => {
+        const lenis = new Lenis();
+
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
   return (
     <div>
       <Landing />
